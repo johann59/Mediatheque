f91bcfbb34f1b941f92c7255f4f0f0fcf586b2ac
Gaetan Bouget <bouget.g@gmail.com>
2017-03-16 09:41:47 +0100

    Ajout de la table book

---
+++ sql/T_book_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO (book) VALUES (1, "Le Livre de la jungle", "Enfant");
+INSERT INTO (book) VALUES (2, "Le Hobbit", "Fantastique");
+INSERT INTO (book) VALUES (3, "La Romance à l'eau de rose", "Romance");
+INSERT INTO (book) VALUES (4, "Franklin", "Enfant");
---
+++ sql/T_book_struct.sql
@@ -0,0 +1,6 @@
+CREATE TABLE books {
+	id INT(4) PRIMARY KEY,
+	name VARCHAR(50),
+	category VARCHAR(50),
+	PRIMARY KEY (id)
+};
